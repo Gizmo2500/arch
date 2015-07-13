894316f88c987007c25f4e97cdc57dd07656a502
David Spencer <davidspencer@Davids-MacBook-Pro-4.local>
2015-07-13 14:40:13 -0700

    update Gemlock file

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20150713010712) do
 
-ActiveRecord::Schema.define(version: 20150711235731) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "sessions", force: :cascade do |t|
     t.string   "name"
@@ -26,12 +28,6 @@ ActiveRecord::Schema.define(version: 20150711235731) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
   end
-
-end
-ActiveRecord::Schema.define(version: 20150713010712) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -50,6 +46,5 @@ ActiveRecord::Schema.define(version: 20150713010712) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
 
 end
