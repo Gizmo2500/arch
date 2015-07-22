3b9498348277478b2cebf5a032774ffc2c1ea268
Camilo Ordonez <icoj95@gmail.com>
2015-07-21 20:38:54 -0700

    Added time to posts and annotations

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -77,6 +77,13 @@ $(document).ready(function() {
 
 //--------------
 
+$(document).on("click", '.open', function(){
+
+
+  // location.reload();
+
+  console.log("Cami");
+});
 
 //-------------------------Add Markups-------------------------
 $('.pencil').on("click", function(e){
@@ -115,38 +122,98 @@ $('.pencil').on("click", function(e){
    $(document).on("click",".image",function(e){
       var x = e.pageX + 'px';
       var y = e.pageY + 'px';
-      bootbox.prompt("Add Post", function(message) {                
-         if (message !== null && message !== ""){
-            completePost = createPostHash(message,x,y);
-            createPostToDB(completePost); 
+      // bootbox.prompt("Add Post", function(message) {                
+      //    if (message !== null && message !== ""){
+      //       completePost = createPostHash(message,x,y);
+      //       createPostToDB(completePost); 
             
-         }
-      });      
+      //    }
+      // });     
+
+      bootbox.dialog({
+                title: "Add a Post",
+                message: '<div class="row">  ' +
+                    '<div class="col-md-12"> ' +
+                      '<form class="form-horizontal"> ' +
+                       '<div class="form-group"> ' +
+                         '<label class="col-md-2 control-label" for="name">Title</label> ' +
+                          '<div class="col-md-9"> ' +
+                            '<input id="title" name="title"  class="form-control input-md"> ' +
+                          '</div>'+
+                        '</div>'+
+                        '<div class="form-group"> ' +
+                          '<label class="col-md-2 control-label" for="comment">Comment</label> ' +
+                          '<div class="col-md-9"> ' +
+                            '<input id="comment" name="comment"  class="form-control input-md"> ' +  
+                          '</div> ' +
+                        '</div> ' +
+                    '</form> </div>  </div>',
+                buttons: {
+                    success: {
+                        label: "Save",
+                        className: "btn-success",
+                        callback: function () {
+                            var title = $('#title').val();
+                            var comment = $('#comment').val();
+                            if (title !== null && title !== ""){
+                              completePost = createPostHash(title,comment,x,y);
+                              createPostToDB(completePost); 
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        );
+
+
+
    });
 
-   function createPostHash(message,x,y) {
-      console.log("User:", user.email);
+   function createPostHash(message,comment,x,y) {
       var completePost = {
                creator: user.email,
                coordX: x,
                coordY: y,
                title: message,
                session_id: session.id,
-               user_id: user.id
+               user_id: user.id,
+               comments: comment
             };
       return completePost;
    }
 
+  function convertTime (a){
+    if (a) {
+      var strTime = a.substring(11);
+      var time1 = strTime.substring(0,5);
+      time = parseInt(time1) ;
+      if (time < 6) {
+        time = time + 5;
+        time = time + time1.substring(2);
+        return time;
+      } else {
+        time = time - 7;
+        time = time + time1.substring(2);
+        return time;
+      }
+    }    
+  }
+
    function displayPost (completePost) {
       var user = getUserInitials(completePost.creator);
-      var name = $('<p class="post1" id="'+ completePost.id +'">' + user + " - " + completePost.title + '</p>'); 
+      var time = convertTime(completePost.updated_at);
+      // var time2 = time.getHours()+":"+time.getMinutes();
+      // console.log(time);
+      var name = $('<p class="post1" id="'+ completePost.id +'" title="'+ completePost.title  +' ">' + user + " - " + time+ " - "+completePost.comments +'</p>'); 
       var div = $('<div class="post" >').css({
          "position": "absolute",
          "left": completePost.coordX,
          "top": completePost.coordY
       });
       div.append(name);
-      $(document.body).append(div);       
+      $(document.body).append(div);   
+      // console.log("completePost: ", completePost);    
    }
 
    function getUserInitials(creator){
@@ -158,18 +225,27 @@ $('.pencil').on("click", function(e){
       $.ajax ({
             url : "/sessions/"+  completePost.id + "/posts",
             type : "post",
-            data : {data_value: JSON.stringify(completePost)},
+            data : {data_value: JSON.stringify(completePost)}
          });
       displayPost(completePost);
       location.reload(true);
    }
 
    function editPostToDB(completePost) {      
+    
       $.ajax ({
             url : "/posts/" + completePost.id,
             type : "patch",
-            data : {data_value: JSON.stringify(completePost)}
+            data : {data_value: JSON.stringify(completePost)},
+            success: function(response, status){
+              console.log("Status:",status);
+            }
+         }).done(function(msg){
+          console.log("status 2", msg);
          });
+      if (completePost.coordX !== "") {
+        location.reload(true);
+      }
    }
 
    function deletePostToDB(completePost) {      
@@ -178,24 +254,36 @@ $('.pencil').on("click", function(e){
             type : "delete",
             data : {data_value: JSON.stringify(completePost)}
          });
+      location.reload(true);
    }
 
 // ----------- Edit Posts -----------
    $(document).on("dblclick", ".post",function(){
-      var previousMessage = $(this).children('p').text();
+      // var previousMessage = $(this).children('p').text();
+      var title = $(this).children('p').attr("title");
+      var comment = $(this).children('p').text().substring(5);
+      
       var id = $(this).children('p')[0].id;
       var that = this;
       bootbox.dialog({
          message: '<div class="row">  ' +
                     '<div class="col-md-12"> ' +
                     '<form class="form-horizontal"> ' +
-                    '<div class="form-group"> ' +
-                    '<label class="col-md-2 control-label" for="name">Current Post</label> ' +
-                    '<div class="col-md-9"> ' +
-                    '<input id="name" name="name" type="text" placeholder="'+ previousMessage+'" class="form-control input-md"> ' +
-                    '<span class="help-block">Click on Accept button!</span> </div> ' +
-                    '</div> ' +
-                    '</div>' +
+                      '<div class="form-group"> ' +
+                        '<label class="col-md-2 control-label" for="title">Post</label> ' +
+                        '<div class="col-md-9"> ' +
+                          '<input id="title" name="title" type="text" placeholder="" class="form-control "> ' +
+                              title +
+                        ' </div> ' +
+                      '</div> ' +
+                      '<div class="form-group"> ' +
+                        '<label class="col-md-2 control-label" for="comment">Comment</label> ' +
+                        '<div class="col-md-9"> ' +
+                          '<input id="comment" name="comment" type="text" placeholder="" class="form-control "> ' +
+                              comment +
+                        ' </div> ' +
+                      '</div> ' +
+                    
                     '</form> </div>  </div>',
          title: "Edit this Post",
          buttons: {
@@ -203,13 +291,19 @@ $('.pencil').on("click", function(e){
                label: "Accept",
                className: "btn-success",
                callback: function() {
-                  message = $('#name').val();
-                  if (message !== null && message !== ""){
-                     completePost = { title: message, id: id};
-                     editPostToDB(completePost);
+                  message = $('#title').val();
+                  commentNew = $('#comment').val();
+                  // console.log("comment", comment);
+                  if (message === ""){
+                     message = title;
+                  } if (commentNew === ""){
+                    commentNew = comment;
+                  }
 
+                     completePost = { title: message, id: id, comments: commentNew};
+                     editPostToDB(completePost);
                      $(that).children('p').text(message);   
-                  } 
+                  
                }
             },
             danger: {
@@ -303,9 +397,9 @@ $('.pencil').on("click", function(e){
     });
     
 // ----------------- Calendar (Datepicker) ---------------------------
-  $(document).ready(function(){
+  // $(document).ready(function(){
     $('.datepicker').datepicker();
-  });  
+  // });  
  
  // ----------------- Dropdown Menu (Show Page) ---------------------------
  $('.dropdown-toggle').dropdown();
